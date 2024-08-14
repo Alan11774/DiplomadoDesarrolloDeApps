a47486e9d1d1296862b59a7b12f6a7b7d690b89e
Alan11774 <alanblacksky@gmail.com>
2024-08-14 00:22:52 -0600

    main Cambios para agregar imagen y enviarla a otra activity

--- app/src/main/java/mx/com/yourlawyer/androidbasic12/tarea/AlanRegistroApp.kt
+++ app/src/main/java/mx/com/yourlawyer/androidbasic12/tarea/AlanRegistroApp.kt
@@ -1,11 +1,14 @@
 package mx.com.yourlawyer.androidbasic12.tarea
 
+import android.app.Activity
 import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
 import android.service.autofill.Validators.and
 import android.widget.Button
 import android.widget.CheckBox
 import android.widget.EditText
+import android.widget.ImageView
 import android.widget.RadioButton
 import android.widget.RadioGroup
 import android.widget.Toast
@@ -14,17 +17,24 @@ import mx.com.yourlawyer.androidbasic12.R
 
 class AlanRegistroApp : AppCompatActivity() {
 
+    private lateinit var ivProfile: ImageView
+    private var imageUri: Uri? = null
+    private val PICK_IMAGE_REQUEST = 1
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_alan_registro_app)
-
+        ivProfile = findViewById(R.id.ivProfile)
         val etName = findViewById<EditText>(R.id.etName)
         val etEmail = findViewById<EditText>(R.id.etEmail)
         val etPassword = findViewById<EditText>(R.id.etPassword)
         val rgGender = findViewById<RadioGroup>(R.id.rgGender)
         val cbTerms = findViewById<CheckBox>(R.id.cbTerms)
         val btnSave = findViewById<Button>(R.id.btnSave)
-
+        ivProfile.setOnClickListener {
+            val intent = Intent(Intent.ACTION_PICK)
+            intent.type = "image/*"
+            startActivityForResult(intent, PICK_IMAGE_REQUEST)
+        }
         btnSave.setOnClickListener {
             val name = etName.text.toString()
             val email = etEmail.text.toString()
@@ -44,11 +54,25 @@ class AlanRegistroApp : AppCompatActivity() {
             if ((password.length >= 8) and (termsAccepted == true)) {
                 val intent = Intent(this, ShowResults::class.java).apply {
                     putExtra("USER_INFO", userInfo)
+                    imageUri?.let {
+                        putExtra("USER_IMAGE", it.toString())
+                    }
                 }
                 startActivity(intent)
             } else {
                 Toast.makeText(this, "Password must be at least 8 characters long", Toast.LENGTH_LONG).show()
             }
+        }
+    }
+    // onActivityResult will be called when the user selects an image from the gallery
+    // and the result is returned to the application
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (requestCode == PICK_IMAGE_REQUEST && resultCode == Activity.RESULT_OK && data != null && data.data != null) {
+            imageUri = data.data
+
+            ivProfile.setImageURI(imageUri)
         }
     }
 }
--- app/src/main/java/mx/com/yourlawyer/androidbasic12/tarea/ShowResults.kt
+++ app/src/main/java/mx/com/yourlawyer/androidbasic12/tarea/ShowResults.kt
@@ -1,6 +1,8 @@
 package mx.com.yourlawyer.androidbasic12.tarea
 
+import android.net.Uri
 import android.os.Bundle
+import android.widget.ImageView
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import mx.com.yourlawyer.androidbasic12.R
@@ -9,9 +11,17 @@ class ShowResults : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_show_results)
-
-        val userInfo = intent.getStringExtra("USER_INFO")
+        // Initialize the views from the layout
         val tvResults = findViewById<TextView>(R.id.tvResultName)
+        val ivUserImage = findViewById<ImageView>(R.id.ivProfile)
+        // Get data from previous activity
+        val userInfo = intent.getStringExtra("USER_INFO")
+        val userImageUri = intent.getStringExtra("USER_IMAGE")
+        // Set the data to the views , first the text
+        // then the image
         tvResults.text = userInfo
+        if (userImageUri != null) {
+            ivUserImage.setImageURI(Uri.parse(userImageUri))
+        }
     }
 }
